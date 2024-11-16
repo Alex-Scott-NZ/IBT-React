1aa5d53214d5f26e6cd49d935d988f31064039a0
Alex Scott <alex.scott@ird.govt.nz>
2024-11-16 14:34:14 +1300

    Changes to scrollbar in side navigation panels

--- src/app/layouts/BaseLayout.tsx
+++ src/app/layouts/BaseLayout.tsx
@@ -29,19 +29,19 @@ const BaseLayout: React.FC<BaseLayoutProps> = ({
         <NavigationMenu />
         <SiteWideNotice notificationData={globalSettings?.fGGlobalSettings?.notificationBar} />
 
-        <div className="flex flex-col nav:flex-row nav:justify-between pt-2 nav:pt-2">
-          <aside className="hidden nav:block w-full nav:w-[20%] mb-4 nav:pt-2 nav:pr-4">
-            <div className="nav:sticky nav:bottom-4 nav:top-4 bg-custom-bg">
+        <div className="flex flex-col nav:flex-row nav:justify-between pt-2">
+          <aside className="hidden nav:block w-full nav:w-[20%] mb-4 pl-2 pr-4">
+            <div className="sticky custom-scrollbar nav:top-4 overflow-y-auto max-h-[calc(100vh_-_var(--header-height)_-_var(--footer-height))]">
               {leftSidebar}
             </div>
           </aside>
 
-          <main className="w-full nav:w-[60%] bg-custom-bg nav:p-2 nav:pl-4 nav:pr-4 p-0 overflow-y-auto mb-4">
+          <main className="w-full nav:w-[60%] bg-custom-bg nav:p-2 p-0 overflow-y-auto mb-4">
             {mainContent}
           </main>
 
-          <aside className="hidden nav:block w-full nav:w-[20%] mb-4 nav:pt-2 nav:pl-4">
-            <div className="nav:sticky nav:top-4 bg-custom-bg">
+          <aside className="hidden nav:block w-full nav:w-[20%] mb-4 pl-4">
+            <div className="sticky custom-scrollbar nav:top-4 overflow-y-auto max-h-[calc(100vh_-_var(--header-height)_-_var(--footer-height))]">
               {rightSidebar}
             </div>
           </aside>
