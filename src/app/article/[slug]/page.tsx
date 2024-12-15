513e93e023df8ca9669dd5646c18c1da7fecaf58
Alex Scott <alex.scott@ird.govt.nz>
2024-12-15 15:49:52 +1300

    Remove Journal Navigator for now...

--- src/app/article/[slug]/page.tsx
+++ src/app/article/[slug]/page.tsx
@@ -1,3 +1,38 @@
-export default function ArticlePage() {
-  return null;
-}
+import {
+  GetArticleByUriQuery,
+  useGetArticleByUriQuery,
+  GetGlobalSettingsQuery,
+  useGetGlobalSettingsQuery,
+} from '../../../gql/gql-generated';
+import ArticleLayout from '../../layouts/ArticleLayout';
+import { serverFetch } from '@/gql/query-utils';
+
+const ArticlePage = async ({ params }: { params: { slug: string } }) => {
+  const uri = `/article/${params.slug}/`;
+
+  // Fetch the required data using serverFetch
+  const articleData: GetArticleByUriQuery = await serverFetch(
+    useGetArticleByUriQuery,
+    { variables: { uri }, next: { revalidate: 60 } }
+  );
+  const globalSettingsData: GetGlobalSettingsQuery = await serverFetch(
+    useGetGlobalSettingsQuery,
+    { next: { revalidate: 60 } }
+  );
+
+  // Check if the article exists
+  if (!articleData.article) {
+    return <div>Article not found</div>;
+  }
+
+  // Render the ArticleLayout with the data as props
+  return (
+    <ArticleLayout
+      article={articleData.article}
+      globalSettings={globalSettingsData.globalSettings}
+      slug={params.slug} // Pass the slug down
+    />
+  );
+};
+
+export default ArticlePage;
